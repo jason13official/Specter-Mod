1f497293d1884511aea759a6781a55f19688724a
jason13official <135896906+jason13official@users.noreply.github.com>
2026-08-22 13:08:40 -0400

    Initialize 1.21.1

---
+++ common/src/main/java/io/github/jason13official/examplemod/ExampleMod.java
@@ -0,0 +1,17 @@
+package io.github.jason13official.examplemod;
+
+import io.github.jason13official.monolib.MonoLib;
+import io.github.jason13official.monolib.impl.common.sailing.Sailing;
+import net.minecraft.resources.ResourceLocation;
+
+public class ExampleMod {
+
+  public static void init() {
+
+    Sailing.register(Constants.MOD_ID, MonoLib.createFilename(Constants.MOD_ID, "1.21.1", "1.0.0"));
+  }
+
+  public static ResourceLocation identifier(final String path) {
+    return ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, path);
+  }
+}
