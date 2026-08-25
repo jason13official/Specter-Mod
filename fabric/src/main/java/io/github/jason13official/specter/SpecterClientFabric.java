2730bfcb18ce37f62a43c9fa772b134135680019
jason13official <jason13official@gmail.com>
2026-08-25 13:06:58 -0400

    some screen base

--- fabric/src/main/java/io/github/jason13official/specter/SpecterClientFabric.java
+++ fabric/src/main/java/io/github/jason13official/specter/SpecterClientFabric.java
@@ -2,12 +2,15 @@ package io.github.jason13official.specter;
 
 import io.github.jason13official.specter.impl.client.model.SpecterModel;
 import io.github.jason13official.specter.impl.client.renderer.SpecterRenderer;
+import io.github.jason13official.specter.impl.client.screen.SpecterScreen;
 import io.github.jason13official.specter.impl.common.registry.ModEntities;
 import io.github.jason13official.specter.impl.common.registry.ModItems;
+import io.github.jason13official.specter.impl.common.registry.ModMenus;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.gui.screens.MenuScreens;
 
 public class SpecterClientFabric implements ClientModInitializer {
 
@@ -20,5 +23,7 @@ public class SpecterClientFabric implements ClientModInitializer {
 
     EntityModelLayerRegistry.registerModelLayer(SpecterModel.LAYER_LOCATION, SpecterModel::createBodyLayer);
     EntityRendererRegistry.register(ModEntities.SPECTER, SpecterRenderer::new);
+
+    MenuScreens.register(ModMenus.SPECTER, SpecterScreen::new);
   }
 }
--- forge/src/main/java/io/github/jason13official/specter/SpecterClientForge.java
+++ forge/src/main/java/io/github/jason13official/specter/SpecterClientForge.java
@@ -2,9 +2,12 @@ package io.github.jason13official.specter;
 
 import io.github.jason13official.specter.impl.client.model.SpecterModel;
 import io.github.jason13official.specter.impl.client.renderer.SpecterRenderer;
+import io.github.jason13official.specter.impl.client.screen.SpecterScreen;
 import io.github.jason13official.specter.impl.common.registry.ModEntities;
 import io.github.jason13official.specter.impl.common.registry.ModItems;
+import io.github.jason13official.specter.impl.common.registry.ModMenus;
 import java.util.function.Consumer;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,7 +17,14 @@ public class SpecterClientForge {
 
   public SpecterClientForge(final IEventBus modEventBus) {
 
-    modEventBus.addListener((Consumer<FMLClientSetupEvent>) event -> SpecterClient.init());
+    modEventBus.addListener((Consumer<FMLClientSetupEvent>) event -> {
+      SpecterClient.init();
+
+      event.enqueueWork(() -> {
+
+        MenuScreens.register(ModMenus.SPECTER, SpecterScreen::new);
+      });
+    });
 
     modEventBus.addListener((Consumer<RegisterColorHandlersEvent.Item>) event -> {
       event.register(SpecterClient.DYED_ITEM_COLOR_FN::apply, ModItems.CONDENSED_SPECTER);
